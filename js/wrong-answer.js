9e30c935459009ab33854ce22d2caa8d8ec28ba4
Matthew <lordmat@gmail.com>
2014-08-26 22:13:43 +0100

    Implemented generating of url Implemented test Q button which opens q.html in a new tab Fixed formatting of html on index.html

--- js/wrong-answer.js
+++ js/wrong-answer.js
@@ -2,29 +2,31 @@ $(function() {
 
     $('#btn-generate').click(function() {
         var text = $('#ta-wrong-message').val();
+        var baseURL = location.href.substring(0, location.href.lastIndexOf("/") + 1);
+        var page = 'q.html?q=' + text;
+        $('#q-url').val(baseURL + page);
 
         if (text) {
             $('#form-enter-message').fadeOut(500, function() {
-
-
                 $('#form-url').fadeIn(500, function() {
 
                 })
             });
-
-        }
+        } 
 
     });
-
 
     $('#btn-reset').click(function() {
         var $textarea = $('#ta-wrong-message').val('');
         $('#form-url').fadeOut(500, function() {
             $('#form-enter-message').fadeIn(500, function() {
                 $textarea.focus();
-            })
+            });
         });
-
     });
 
+    $('#btn-test').click(function(){
+       window.open($('#q-url').val(),'_blank');
+        
+    });
 });
